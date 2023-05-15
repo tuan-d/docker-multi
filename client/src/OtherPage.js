54c44eedd88a4da4b1b0772aaa901ea536715cf5
tuan-d <tuandl@nicegroup.com.vn>
2023-05-15 14:36:15 +0700

    push some stuffs

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const OtherPage = () => {
     return (
         <div>
-            Im some other page!
-            <Link to="/">Go back home</Link>
+            Please try again later!
+            <Link to="/"> Go back home</Link>
         </div>
     )
 }
